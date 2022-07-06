6f3be99bbbd5859398596b4f29a3ee840d01ec98
Piumal Rathnayake <piumal1999@gmail.com>
2022-07-06 23:28:51 +0530

    View buttons when marker is detected (#30)

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+AFRAME.registerComponent("floor-0", {
+  init: function () {
+    this.el.addEventListener("markerFound", () => {
+      // TODO: add event listner to view-map button
+      $("#btn-view-map").show();
+      $("#btn-see-more").click(function () {
+        window.location.href = "http://www.ce.pdn.ac.lk/";
+      }).show();
+    });
+    this.el.addEventListener("markerLost", () => {
+      $("#btn-view-map").hide();
+      $("#btn-see-more").hide();
+    });
+  },
+});
+
+AFRAME.registerComponent("room-g01", {
+  init: function () {
+    this.el.addEventListener("markerFound", () => {
+      $("#btn-see-more").click(function () {
+        window.location.href = "http://www.ce.pdn.ac.lk/facilities/networking-lab/";
+      }).show();
+    });
+    this.el.addEventListener("markerLost", () => {
+      $("#btn-see-more").hide();
+    });
+  },
+});
