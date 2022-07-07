d4fe7c02b8f5b85b2cd9f8a46032d4c0c224b9d5
chathurawimalasiri <e18402@eng.pdn.ac.lk>
2022-07-07 14:57:41 +0530

    link the "view map" button to the 2D floor  map.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,12 @@ AFRAME.registerComponent("floor-0", {
   init: function () {
     this.el.addEventListener("markerFound", () => {
       // TODO: add event listner to view-map button
-      $("#btn-view-map").show();
+      $("#btn-view-map")
+        .click(function () {
+          const map_ground_floor = document.querySelector("#map-ground-floor");
+          map_ground_floor.setAttribute("visible", true);
+        })
+        .show();
       $("#btn-see-more").click(function () {
         window.location.href = "http://www.ce.pdn.ac.lk/";
       }).show();
