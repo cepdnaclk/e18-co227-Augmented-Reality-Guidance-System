a3ffafe13031b27a756a7c9c26d745e725df4778
Chathura Thimanka Wimalasiri <73394733+chathurawimalasiri@users.noreply.github.com>
2022-07-07 18:28:01 +0530

    Add the View Map feature  (#35)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,22 @@
 AFRAME.registerComponent("floor-0", {
   init: function () {
     this.el.addEventListener("markerFound", () => {
-      // TODO: add event listner to view-map button
-      $("#btn-view-map").show();
-      $("#btn-see-more").click(function () {
-        window.location.href = "http://www.ce.pdn.ac.lk/";
-      }).show();
+      $("#btn-view-map")
+        .click(function () {
+          if ($(this).text() === "Hide Map") {
+            $("#map-ground-floor").attr("visible", false);
+            $(this).text("View Map");
+          } else {
+            $("#map-ground-floor").attr("visible", true);
+            $(this).text("Hide Map");
+          }
+        })
+        .show();
+      $("#btn-see-more")
+        .click(function () {
+          window.location.href = "http://www.ce.pdn.ac.lk/";
+        })
+        .show();
     });
     this.el.addEventListener("markerLost", () => {
       $("#btn-view-map").hide();
@@ -17,9 +28,12 @@ AFRAME.registerComponent("floor-0", {
 AFRAME.registerComponent("room-g01", {
   init: function () {
     this.el.addEventListener("markerFound", () => {
-      $("#btn-see-more").click(function () {
-        window.location.href = "http://www.ce.pdn.ac.lk/facilities/networking-lab/";
-      }).show();
+      $("#btn-see-more")
+        .click(function () {
+          window.location.href =
+            "http://www.ce.pdn.ac.lk/facilities/networking-lab/";
+        })
+        .show();
     });
     this.el.addEventListener("markerLost", () => {
       $("#btn-see-more").hide();
