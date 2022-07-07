390af2b951aab3c19ec6215788992c6d2e934240
chathurawimalasiri <e18402@eng.pdn.ac.lk>
2022-07-07 17:29:11 +0530

    change <a-entity> ti<a-image> for 2D map.

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,13 +4,14 @@ AFRAME.registerComponent("floor-0", {
       // TODO: add event listner to view-map button
       $("#btn-view-map")
         .click(function () {
-          const map_ground_floor = document.querySelector("#map-ground-floor");
-          map_ground_floor.setAttribute("visible", true);
+          $("#map-ground-floor").attr("visible", true);
         })
         .show();
-      $("#btn-see-more").click(function () {
-        window.location.href = "http://www.ce.pdn.ac.lk/";
-      }).show();
+      $("#btn-see-more")
+        .click(function () {
+          window.location.href = "http://www.ce.pdn.ac.lk/";
+        })
+        .show();
     });
     this.el.addEventListener("markerLost", () => {
       $("#btn-view-map").hide();
@@ -22,9 +23,12 @@ AFRAME.registerComponent("floor-0", {
 AFRAME.registerComponent("room-g01", {
   init: function () {
     this.el.addEventListener("markerFound", () => {
-      $("#btn-see-more").click(function () {
-        window.location.href = "http://www.ce.pdn.ac.lk/facilities/networking-lab/";
-      }).show();
+      $("#btn-see-more")
+        .click(function () {
+          window.location.href =
+            "http://www.ce.pdn.ac.lk/facilities/networking-lab/";
+        })
+        .show();
     });
     this.el.addEventListener("markerLost", () => {
       $("#btn-see-more").hide();
